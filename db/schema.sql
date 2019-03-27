b11279a8bf207dff696c3425aedaaaa68d53a093
kbefff <kbeffel10@me.com>
2019-03-27 14:00:40 -0700

    schema and seed revision

--- db/schema.sql
+++ db/schema.sql
@@ -1,16 +1,18 @@
 
 --    * Create the `burgers_db`.
 -- 2. In the `db` folder, create a file named `schema.sql`. Write SQL queries this file that do the following:
-
+-- DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 --    * Switch to or use the `burgers_db`.
 USE burgers_db;
 --    * Create a `burgers` table with these fields:
 CREATE TABLE burgers(
 --      * **id**: an auto incrementing int that serves as the primary key.
-    id int NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+    id int NOT NULL AUTO_INCREMENT,
 --      * **burger_name**: a string.
-    buyer_name varchar(255) NOT NULL,
+    burger_name varchar(255) NOT NULL,
 --      * **devoured**: a boolean.
     devoured BOOLEAN DEFAULT false,   
+	PRIMARY KEY (id)
 );
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,8 @@
 -- 3. Still in the `db` folder, create a `seeds.sql` file. In this file, write insert queries to populate the `burgers` table with at least three entries.
 
-INSERT INTO burgers(buyer_name, devoured)
-VALUES ("Kelsey", true),
-("Haley", false),
-("Kyle", true);
+INSERT INTO burgers(burger_name)
+VALUES ("Fancy Burger"),
+("Super Fancy Burger"),
+("The Fanciest Burger");
+
+SELECT * FROM burgers;
