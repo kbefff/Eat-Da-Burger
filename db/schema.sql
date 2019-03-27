0b62328f4c506c049a7310219c34990fc68bd7e8
kbefff <kbeffel10@me.com>
2019-03-27 13:42:14 -0700

    schema

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+
+--    * Create the `burgers_db`.
+CREATE DATABASE burgers_db;
+--    * Switch to or use the `burgers_db`.
+USE burgers_db;
+--    * Create a `burgers` table with these fields:
+CREATE TABLE burgers(
+--      * **id**: an auto incrementing int that serves as the primary key.
+--      * **burger_name**: a string.
+--      * **devoured**: a boolean.
+
+)
