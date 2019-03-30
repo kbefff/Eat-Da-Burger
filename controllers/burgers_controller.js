281d7b31fecfe6975efb4de905273ee74afbed40
kbefff <kbeffel10@me.com>
2019-03-29 21:01:56 -0700

    ctonroller routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -35,33 +35,33 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/api/cats", function(req, res) {
-  cat.create(["name", "sleepy"], [req.body.name, req.body.sleepy], function(result) {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
-  });
-});
+// router.post("/api/cats", function(req, res) {
+//   cat.create(["name", "sleepy"], [req.body.name, req.body.sleepy], function(result) {
+//     // Send back the ID of the new quote
+//     res.json({ id: result.insertId });
+//   });
+// });
 
-router.put("/api/cats/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+// router.put("/api/cats/:id", function(req, res) {
+//   var condition = "id = " + req.params.id;
 
-  console.log("condition", condition);
+//   console.log("condition", condition);
 
-  cat.update(
-    {
-      sleepy: req.body.sleepy
-    },
-    condition,
-    function(result) {
-      if (result.changedRows === 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      }
-      res.status(200).end();
+//   cat.update(
+//     {
+//       sleepy: req.body.sleepy
+//     },
+//     condition,
+//     function(result) {
+//       if (result.changedRows === 0) {
+//         // If no rows were changed, then the ID must not exist, so 404
+//         return res.status(404).end();
+//       }
+//       res.status(200).end();
 
-    }
-  );
-});
+//     }
+//   );
+// });
 
 // Export routes for server.js to use.
 module.exports = router;
