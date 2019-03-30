47ffa3d77643e69d428253789dff6292f968c238
kbefff <kbeffel10@me.com>
2019-03-29 21:00:18 -0700

    burger controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -8,3 +8,60 @@
 //    * `burger.js`
 
 // 4. Create the `router` for the app, and export the `router` at the end of your file.
+
+
+var express = require("express");
+// Using express.Router() allows us to define routes to be used by our app.
+// https://expressjs.com/en/guide/routing.html#express-router
+/*`
+  e,g, in server.js
+  // Import routes and give the server access to them.
+  var routes = require("./controllers/catsController.js");
+  app.use(routes);
+*/
+var router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+var cat = require("../models/cat.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  cat.all(function(data) {
+    var hbsObject = {
+      cats: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/cats", function(req, res) {
+  cat.create(["name", "sleepy"], [req.body.name, req.body.sleepy], function(result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/cats/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  cat.update(
+    {
+      sleepy: req.body.sleepy
+    },
+    condition,
+    function(result) {
+      if (result.changedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      }
+      res.status(200).end();
+
+    }
+  );
+});
+
+// Export routes for server.js to use.
+module.exports = router;
